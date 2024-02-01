89b302146627e59d9cecc6765a394ed9a596a945
han18 <han.fadah@gmail.com>
2024-02-01 11:45:45 -0500

    navigating between login or sign up

--- src/Pages/MainPage.js
+++ src/Pages/MainPage.js
@@ -1,37 +1,111 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 function MainPage() {
   const emailInputRef = useRef(null);
   const passwordInputRef = useRef(null);
+  const [showSignUp, setShowSignUp] = useState(false);
+
+  const handleSignIn = (e) => {
+    e.preventDefault();
+    console.log(emailInputRef.current.value);
+    console.log(passwordInputRef.current.value);
+
+    if (emailInputRef.current.value === "") {
+      emailInputRef.current.focus();
+      return;
+    }
+    if (passwordInputRef.current.value === "") {
+      passwordInputRef.current.focus();
+      return;
+    }
+  };
+
+  const handleSignUp = (e) => {
+    e.preventDefault();
+    console.log(emailInputRef.current.value);
+    console.log(passwordInputRef.current.value);
+
+    if (emailInputRef.current.value === "") {
+      emailInputRef.current.focus();
+      return;
+    }
+    if (passwordInputRef.current.value === "") {
+      passwordInputRef.current.focus();
+      return;
+    }
+  };
 
   return (
     <main>
       <h1>Main Page</h1>
 
-      <div>
-        <form
-          style={{ display: "flex", flexDirection: "column", padding: "10px" }}
-        >
-          <h3>Sign In</h3>
-          <label htmlFor="email">Email</label>
-          <input
-            ref={emailInputRef}
-            name="email"
-            id="email"
-            type="text"
-            placeholder="Email or phone"
-          />
-          <label htmlFor="password">Password</label>
-          <input
-            ref={passwordInputRef}
-            name="password"
-            id="password"
-            type="text"
-            placeholder="Email or phone"
-          />
-          <button type="submit">Sign In</button>
-        </form>
-      </div>
+      {showSignUp ? (
+        <div>
+          <form
+            onSubmit={handleSignIn}
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              padding: "10px",
+            }}
+          >
+            <h3>Sign In</h3>
+            <label htmlFor="email">Email</label>
+            <input
+              ref={emailInputRef}
+              name="email"
+              id="email"
+              type="text"
+              placeholder="Email or phone"
+            />
+            <label htmlFor="password">Password</label>
+            <input
+              ref={passwordInputRef}
+              name="password"
+              id="password"
+              type="text"
+            />
+            <button type="submit">Sign In</button>
+          </form>
+          <span>
+            Don't have an account?{" "}
+            <button onClick={() => setShowSignUp(!showSignUp)}>Sign Up</button>
+          </span>
+        </div>
+      ) : (
+        <div>
+          <form
+            onSubmit={handleSignUp}
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              padding: "10px",
+            }}
+          >
+            <h3>Sign Up</h3>
+            <label htmlFor="email">Email</label>
+            <input
+              ref={emailInputRef}
+              name="email"
+              id="email"
+              type="text"
+              placeholder="Email or phone"
+            />
+            <label htmlFor="password">Password</label>
+            <input
+              ref={passwordInputRef}
+              name="password"
+              id="password"
+              type="text"
+            />
+            <button type="submit">Sign Up</button>
+          </form>
+          <span>
+            Already have an account?{" "}
+            <button onClick={() => setShowSignUp(!showSignUp)}>Sign In</button>
+          </span>
+        </div>
+      )}
     </main>
   );
 }
